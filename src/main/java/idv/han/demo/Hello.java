56208a1a9bfbd25f37f9a7c6e02d1a4bafb2aff3
Han Huang <null>
2021-04-11 03:17:44 +0800

    modification for Nginx - Reverse proxy and Load Balancer

--- src/main/java/idv/han/demo/Hello.java
+++ src/main/java/idv/han/demo/Hello.java
@@ -1,10 +1,17 @@
 package idv.han.demo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
+    
+    /* 注入“服务提供者”的端口号 */
+    // @Value("${provider.port}")
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/hello")
     public String hello()  throws Exception{
-        return "Hello ,Spring Boot!";
+        return "Hello ,Spring Boot! port:" + port;
     }
 }
